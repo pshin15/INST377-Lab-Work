cc535452bbfa346935d1e09673beeeb418cd317e
Paula Shin <paulashin15@gmail.com>
2023-02-19 23:08:25 -0500

    added movetoprevslide queryselector

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -57,10 +57,10 @@ function moveToPrevSlide() {
     if you're already at the first index position for an array
     and if so, sets your slidePosition to the last slide position in totalSlides
     if not, set the slidePosition to the current position minus one
-  */
+    */
 
   if(slidePosition === 0){
-    slidePosition = totalSlides-1;
+    slidePosition = totalSlides - 1;
   } else { 
     slidePosition -= 1;
   }
@@ -78,5 +78,10 @@ document.querySelector('.next') // Get the appropriate element (<button class="n
     moveToNextSlide(); // call the function above to handle this
   });
 
+  document.querySelector('.prev') // Get the appropriate element (<button class="next">)
+  .addEventListener('click', () => { // set an event listener on it - when it's clicked, do this callback function
+    console.log('clicked prev'); // let's tell the client console we made it to this point in the script
+    moveToPrevSlide(); // call the function above to handle this
+  });
 // Paying close attention to the above queryselector, write one that fires
 // when you want a "prev" slide
